7997356910139530a0f3e34babebf06addaa3c02
abi-y-shimizu <yuuki.shimizu@abi-inc.co.jp>
2024-12-24 13:13:18 +0900

    desknetsから出社日取得まで完了 長期休暇への対応は未完了

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { getWorkDayFromDesknets } from "./getWorkDayFromDesknets";
 
 test("desknetsから出社営業日を取得し楽々清算にて申請を一時保存", async ({ page }) => {
     test.setTimeout(600000);
-    await getWorkDayFromDesknets({ page });
+    const officeDateList = await getWorkDayFromDesknets({ page });
 });
