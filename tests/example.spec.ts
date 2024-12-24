fcad5b5ce69de74ab026422ae3586985bf1d46b4
abi-y-shimizu <yuuki.shimizu@abi-inc.co.jp>
2024-12-24 16:34:29 +0900

    desknets取得 作成完了

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,18 @@
 import { test } from "@playwright/test";
-import { getWorkDayFromDesknets } from "./getWorkDayFromDesknets";
+import { getOfficeDayFromDesknets } from "./getOfficeDayFromDesknets";
 
 test("desknetsから出社営業日を取得し楽々清算にて申請を一時保存", async ({ page }) => {
-    test.setTimeout(600000);
-    const officeDateList = await getWorkDayFromDesknets({ page });
+    try{
+        console.log('==== test start ====')
+        test.setTimeout(600000);
+        const result = await getOfficeDayFromDesknets({ page });
+        const officeDates = result![0];
+        const remoteDates = result![1];
+        const vacationDates = result![2];
+        console.log('officeDates:', officeDates);
+    }catch(error){
+        console.error('test Error:',error);
+    }finally{
+        console.log('==== test end ====')
+    }
 });
